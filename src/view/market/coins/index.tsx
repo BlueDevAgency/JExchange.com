6ad9ad5985f8244a3b04abc9e2e7f13f9b6e67a4
Masoud Golmohammadi <golmohammadi.masoud@gmail.com>
2024-01-10 02:04:54 +0330

    feat: add filter button

--- src/view/market/coins/index.tsx
+++ src/view/market/coins/index.tsx
@@ -6,6 +6,8 @@ import CircleChart from "../../../assets/icon/circle-chart.svg";
 import BitCoin from "../../../assets/icon/bitcoin.svg";
 import Cake from "../../../assets/icon/cake.svg";
 import LiteCoin from "../../../assets/icon/litecoin.svg";
+import FilterIcon from "../../../assets/icon/Filter.svg";
+import ArrowDown from "../../../assets/icon/arrow-down.svg";
 
 const MarketCoinsView = () => {
     const cards = [
@@ -61,13 +63,24 @@ const MarketCoinsView = () => {
                 className={`
                     w-full h-full
                     px-20
-                    bg-zinc-800
                     2xl: h-screen
                     sm:pt-10 pb-20
                     md:pt-10 pb-20
+                    -mt-60
                 `}
             >
-                <div className="scroll-smooth h-screen">
+                <div className="scroll-smooth h-screen space-y-8">
+                    <div className="w-full flex flex-row-reverse space-x-4">
+                        <div className="flex space-x-2 items-center px-3.5 py-2 border border-solid border-white/[0.5] rounded-xl	">
+                            <img className="w-4 h-4" src={FilterIcon} alt="Filter" />
+                            <span className="text-white font-medium	text-xs">Filter</span>
+                        </div>
+
+                        <div className="flex space-x-0 items-center mr-8">
+                            <span className="text-white font-medium	text-xs">Past 7 days</span>
+                            <img className="w-6 h-6" src={ArrowDown} alt="Filter" />
+                        </div>
+                    </div>
                     <div
                         className={`
                                     w-full
